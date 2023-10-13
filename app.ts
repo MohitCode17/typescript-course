b1e0dc766bba3d36911b147b77daba00148b7c51
MohitGupta <krishgupta1630@gmail.com>
2023-10-13 20:12:33 +0530

    lec02 typescript

--- app.ts
+++ app.ts
@@ -1,19 +1,48 @@
-// -------------- LEC-01 INTRO OF TYPESCRIPT
+// -------------- LEC-02 Types in TypeScript --------------
 
-// 👉 What is TypeScript ?
+// 👉 What is Types ?
 
-// TypeScript is typed JavaScript. TypeScript adds types to JavaScript to help you speed up the development by catching errors before you even run the JavaScript code.
-
-// TypeScript is an programming language that builds on top of JavaScript. It works on any web browser, any OS, and any environment that JavaScript runs.
-
-// 👉 Why TypeScript :-
-
-// ➡️ TypeScript improves your productivity while helping avoid bugs
-
-// ➡️ TypeScript brings the future JavaScript to today, TypeScript supports the upcoming features planned in the ES Next for the current JavaScript engines.
+// A type is a label that describes the different properties and methods that a value has. A value is anything that you can assign to a variable e.g., a number, a string, an array, an object, and a function.
 
 
-// 👉 Your First Code in TypeScript
+// 👉 Types in TypeScript:-
 
-let greet: string = 'Hyy Everyone!!';
-console.log(greet);
+// ➡️ Primitive types: String, Number, Boolean, Null, Undefined, Symbol.
+
+// ➡️ Object types: Functions, Arrays, Classes, etc.
+
+
+// 👉 Type Annotations
+
+// TypeScript uses type annotations to explicitly specify types for identifiers.
+
+// TypeScript uses the syntax : type after an identifier
+
+// Syntax: let variableName: type;
+
+let count: number = 20;
+count = 21;
+// count = 'hello';   // compile error: 'string' is not assignable to type 'number'
+
+let isActive: boolean = true;
+let studentName: string = 'Rohan';
+
+
+// 👉 Type Inference
+
+// Type inference describes where and how TypeScript infers types when you don’t explicitly annotate them.
+
+// let counter: number = 10;   // Explicitly specify a type
+
+let counter = 10;   // If we initialize the counter variable with a number, TypeScript will infer the type the to be 'number'.
+
+
+/*
+👉 ------------------ NOTED ----------------------
+
+In practice, you should always use the type inference as much as possible. And you use the type annotation in the following cases:
+
+➡️ When you declare a variable and assign it a value later.
+➡️ When you want a variable that can’t be inferred.
+➡️When a function returns the any type and you need to clarify the value.
+*/
