6105d98b5624fa22eba2166ecf0fcdb1a3ca6184
MohitGupta <krishgupta1630@gmail.com>
2023-10-13 18:12:46 +0530

    lec01 typescript

---
+++ app.js
@@ -0,0 +1,10 @@
+// -------------- LEC-01 INTRO OF TYPESCRIPT
+// 👉 What is TypeScript ?
+// TypeScript is typed JavaScript. TypeScript adds types to JavaScript to help you speed up the development by catching errors before you even run the JavaScript code.
+// TypeScript is an programming language that builds on top of JavaScript. It works on any web browser, any OS, and any environment that JavaScript runs.
+// 👉 Why TypeScript :-
+// ➡️ TypeScript improves your productivity while helping avoid bugs
+// ➡️ TypeScript brings the future JavaScript to today, TypeScript supports the upcoming features planned in the ES Next for the current JavaScript engines.
+// 👉 Your First Code in TypeScript
+var greet = 'Hyy Everyone!!';
+console.log(greet);
